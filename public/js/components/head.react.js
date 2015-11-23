b69763b0863b14ab6e11a62d510bf4cbd1398778
Tahir J <icodejs@gmail.com>
2015-11-23 15:07:57 +0000

    Update examnple for Jest blog post

--- public/js/components/head.react.js
+++ public/js/components/head.react.js
@@ -2,9 +2,24 @@
 import React from 'react';
 
 export default class extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { showDiv: false };
+    }
+
     _handleClick(e) {
         e.preventDefault();
-        alert('You clicked me!');
+        this.setState({ showDiv: true });
+    }
+
+    _renderDiv() {
+        let text = '';
+
+        if (this.state.showDiv) text = 'You clicked me!';
+
+        return (
+            <div className="my-div">{text}</div>
+        );
     }
 
     render() {
@@ -12,6 +27,7 @@ export default class extends React.Component {
             <div>
                 <h1>Universal JavaScript Example</h1>
                 <button onClick={this._handleClick.bind(this)}>Click</button>
+                {this._renderDiv()}
                 <hr />
             </div>
         );
